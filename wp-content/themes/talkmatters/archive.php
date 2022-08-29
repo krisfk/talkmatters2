0777b38e5815fb72d03041e27a6a4dbc28cb7c8f
kaiyu li <krisfk@gmail.com>
2022-08-30 04:59:25 +0800

    .

--- wp-content/themes/talkmatters/archive.php
+++ wp-content/themes/talkmatters/archive.php
@@ -174,38 +174,23 @@ else
 
                 <div class="pagination">
                     <?php 
-        // echo paginate_links( array(
-        //     'base'         => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
-        //     'total'        => $query->max_num_pages,
-        //     'current'      => max( 1, get_query_var( 'paged' ) ),
-        //     'format'       => '?paged=%#%',
-        //     'show_all'     => false,
-        //     'type'         => 'plain',
-        //     'end_size'     => 2,
-        //     'mid_size'     => 1,
-        //     'prev_next'    => true,
-        //     'prev_text'    => sprintf( '<i></i> %1$s', __( '<', 'text-domain' ) ),
-        //     'next_text'    => sprintf( '%1$s <i></i>', __( '>', 'text-domain' ) ),
-        //     'add_args'     => false,
-        //     'add_fragment' => '',
-        // ) );
+        echo paginate_links( array(
+            'base'         => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
+            'total'        => $query->max_num_pages,
+            'current'      => max( 1, get_query_var( 'paged' ) ),
+            'format'       => '?paged=%#%',
+            'show_all'     => false,
+            'type'         => 'plain',
+            'end_size'     => 2,
+            'mid_size'     => 1,
+            'prev_next'    => true,
+            'prev_text'    => sprintf( '<i></i> %1$s', __( '<', 'text-domain' ) ),
+            'next_text'    => sprintf( '%1$s <i></i>', __( '>', 'text-domain' ) ),
+            'add_args'     => false,
+            'add_fragment' => '',
+        ) );
 
-    //     $num = 999999;
-
-	// $pagination = paginate_links( array(
-	// 	'base' => str_replace( $num, '%#%', esc_url( get_pagenum_link( $num ) ) ),
-	// 	'format' => '?paged=%#%',
-	// 	'current' => max( 1, get_query_var('paged') ),
-	// 	'total' => $query->max_num_pages,
-	// 	'prev_text' => 'Vorige',
-	// 	'next_text' => 'Volgende',
-	// ) );
-
-	// if( $pagination ) {
-	// 	echo "<div id='pager' class='arial clearfix'>{$pagination}</div>";
-	// } else {
-	// 	echo '';
-	// }
+   
     ?>
 
 
