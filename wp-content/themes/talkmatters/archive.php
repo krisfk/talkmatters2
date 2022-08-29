3a14c64708358f8e28c353e10f2fb5acfbedde89
kaiyu li <krisfk@gmail.com>
2022-08-30 04:16:09 +0800

    .

--- wp-content/themes/talkmatters/archive.php
+++ wp-content/themes/talkmatters/archive.php
@@ -174,21 +174,21 @@ else
 
                 <div class="pagination">
                     <?php 
-        echo paginate_links( array(
-            'base'         => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
-            'total'        => $query->max_num_pages,
-            'current'      => max( 1, get_query_var( 'paged' ) ),
-            'format'       => '?paged=%#%',
-            'show_all'     => false,
-            'type'         => 'plain',
-            'end_size'     => 2,
-            'mid_size'     => 1,
-            'prev_next'    => true,
-            'prev_text'    => sprintf( '<i></i> %1$s', __( '<', 'text-domain' ) ),
-            'next_text'    => sprintf( '%1$s <i></i>', __( '>', 'text-domain' ) ),
-            'add_args'     => false,
-            'add_fragment' => '',
-        ) );
+        // echo paginate_links( array(
+        //     'base'         => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
+        //     'total'        => $query->max_num_pages,
+        //     'current'      => max( 1, get_query_var( 'paged' ) ),
+        //     'format'       => '?paged=%#%',
+        //     'show_all'     => false,
+        //     'type'         => 'plain',
+        //     'end_size'     => 2,
+        //     'mid_size'     => 1,
+        //     'prev_next'    => true,
+        //     'prev_text'    => sprintf( '<i></i> %1$s', __( '<', 'text-domain' ) ),
+        //     'next_text'    => sprintf( '%1$s <i></i>', __( '>', 'text-domain' ) ),
+        //     'add_args'     => false,
+        //     'add_fragment' => '',
+        // ) );
     ?>
                 </div>
 
@@ -239,7 +239,7 @@ else
 
 
     </div>
-    test
+
 </div>
 <?php
 get_footer();
